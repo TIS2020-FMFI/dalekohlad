95a8743a414b4df483ca0581e133bc12a5fae1e3
nofiv <41523109+nofiv@users.noreply.github.com>
2020-10-21 12:13:09 +0200

    Added a lock enforcing only one instance of the application to run at a time and loading of initial FXML

--- src/main/java/fmfi/dalekohlad/Main.java
+++ src/main/java/fmfi/dalekohlad/Main.java
@@ -1,31 +1,39 @@
 package fmfi.dalekohlad;
 
-import fmfi.dalekohlad.Config.Config;
+import fmfi.dalekohlad.LockInstance.LockInstance;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main extends Application {
     private static final Logger lgr = LogManager.getLogger(Main.class);
+    private static final String main_screen = "/fxml/main_screen.fxml";
 
     @Override
     public void start(Stage stage) {
-        lgr.debug("Initial test");
-        System.out.println("This will be printed");
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        LockInstance.lock_instance();
+        load_scene(stage);
     }
 
     public static void main() {
         launch();
+    }
+
+    private void load_scene(Stage stage) {
+        Scene scene;
+        try {
+            scene = FXMLLoader.load(getClass().getResource(main_screen));
+        }
+        catch (Exception e) {
+            lgr.fatal("Unable to load FXML resource: " + main_screen, e);
+            return;
+        }
+        stage.setScene(scene);
+        stage.show();
+        lgr.debug("Loaded FXML " + main_screen);
     }
 
 }
