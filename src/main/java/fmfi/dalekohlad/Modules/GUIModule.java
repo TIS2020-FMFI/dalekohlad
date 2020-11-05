78ac2cf893410edd91a05cd7fe87ba47fed21168
Vladislav Hrčka <41523109+nofiv@users.noreply.github.com>
2020-11-05 21:56:51 +0100

    update poziadaviek na moduly

--- src/main/java/fmfi/dalekohlad/Modules/GUIModule.java
+++ src/main/java/fmfi/dalekohlad/Modules/GUIModule.java
@@ -7,10 +7,18 @@ import javafx.util.Pair;
 import java.util.Map;
 
 public interface GUIModule {
-    // vsetky moduly by mali mat default konstruktor
-    // kazdy ma v inite prideleny prislusny Pane, ktory moze lubovolne upravovvat
-    // Pane prisluchajuci urcitemu modulu musi byt v GUI priamym potomkom sceny a mat id v tvare "module_%nazov_modulu%"
-    // update bude volany vzdy ked sa dostane v komunikacii novy konfig
+    // Vsetky moduly by mali mat default konstruktor.
+    
+    // Kazdy modul ma v inite prideleny prislusny Pane, ktory moze lubovolne upravovat.
+    
+    // Pane prisluchajuci urcitemu modulu musi byt v GUI priamym potomkom sceny a mat id v tvare "module_%nazov_modulu%".
+    
+    // update bude volany vzdy ked sa dostane v komunikacii novy konfig.
+    
+    // Kazdy modul musi robit zmeny v GUI pomocou Platform.runLater() alebo Task (https://stackoverflow.com/questions/13784333/platform-runlater-and-task-in-javafx),
+    // napriklad: Platform.runLater(() -> {label.text = "zmeneny";});.
+    // Je to nevyhnutne kvoli https://stackoverflow.com/questions/25171039/what-is-the-best-way-to-manage-multithreading-in-javafx-8       .
+    
     void update(String data);
     void init(Pane pane);
     String collectData();
