8ab061b77049446bd5da2a53d2b45e480de07d6e
muriga <milos.uriga313@gmail.com>
2020-11-17 06:54:32 +0100

    gradle library nom.tam fixed

--- src/main/java/fmfi/dalekohlad/Modules/LastFrame.java
+++ src/main/java/fmfi/dalekohlad/Modules/LastFrame.java
@@ -1,17 +1,20 @@
 package fmfi.dalekohlad.Modules;
 
 import com.google.gson.JsonObject;
+import fmfi.dalekohlad.Main;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.util.Pair;
 
 import java.util.Map;
 
-//import nom.tam.fits;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import nom.tam.fits.Fits;
 
 public class LastFrame implements GUIModule{
     private Pane pane;
+    private static final Logger lgr = LogManager.getLogger(LastFrame.class);
 
     @Override
     public void update(JsonObject jo) {
@@ -19,7 +22,13 @@ public class LastFrame implements GUIModule{
 
     @Override
     public void init(Pane pane) {
+
         this.pane = pane;
+        try {
+            Fits f = new Fits("/example.fits");
+        } catch (Exception e) {
+            lgr.fatal(e.getMessage());
+        }
     }
 
     @Override
