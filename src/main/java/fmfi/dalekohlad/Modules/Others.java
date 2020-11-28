1e9da9dfe60651fe932497af8e0da9f29fe315df
muriga <milos.uriga313@gmail.com>
2020-11-28 18:00:07 +0100

    shortcuts scene added, refactor necessary #36

--- src/main/java/fmfi/dalekohlad/Modules/Others.java
+++ src/main/java/fmfi/dalekohlad/Modules/Others.java
@@ -5,11 +5,16 @@ import eap.fits.FitsHDU;
 import eap.fits.FitsImageData;
 import eap.fits.RandomAccessFitsFile;
 import eap.fitsbrowser.FitsImageViewer;
+import fmfi.dalekohlad.Mediator;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingNode;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,7 +22,10 @@ import java.awt.*;
 import java.io.RandomAccessFile;
 
 public class Others implements GUIModule {
+    private static final String shortcuts_screen = "/fxml/shortcuts_scene.fxml";
+    private Parent main_screen_root;
     private Pane pane;
+    private Scene ultimateScene;
     private Button shortcuts;
     private Label info;
     private Label status;
@@ -61,6 +69,28 @@ public class Others implements GUIModule {
         });
     }
 
+    private void displayShortcuts() {
+        VBox parent = null;
+        try {
+            parent = FXMLLoader.load(Others.class.getResource(shortcuts_screen));
+        }
+        catch (Exception e) {
+            lgr.fatal("Unable to load FXML resource: " + shortcuts_screen, e);
+            System.exit(1);
+        }
+        ultimateScene = pane.getParent().getScene();
+        pane.getParent().getScene().setRoot(parent);
+
+        Pane otherPane = (Pane) parent.getChildren().get(0);
+        Button back = (Button) GUIModule.GetById(otherPane, "back");
+        back.setOnAction(e -> dislapyMain());
+        lgr.debug("Loaded FXML " + shortcuts_screen);
+    }
+
+    private void dislapyMain() {
+        ultimateScene.setRoot(main_screen_root);
+    }
+
     @Override
     public void init(Pane pane) {
         this.pane = pane;
@@ -70,7 +100,8 @@ public class Others implements GUIModule {
         this.status = (Label) GUIModule.GetById(pane, "Connected");
         this.pathFITS = (Label) GUIModule.GetById(pane, "path_to_last_frame");
         this.swingNode = (SwingNode) GUIModule.GetById(pane, "node_FITS");
-        
-        //((Button) GUIModule.GetById(pane,"Shortcuts")).setOnAction(event -> this.update(new JsonObject()));
+        this.main_screen_root = pane.getParent().getScene().getRoot();
+
+        ((Button) GUIModule.GetById(pane,"Shortcuts")).setOnAction(event -> this.displayShortcuts());
     }
 }
