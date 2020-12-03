9ba581bee31f8c8003d57b9a725cd939c11a5261
nofiv <41523109+nofiv@users.noreply.github.com>
2020-12-04 00:27:27 +0100

    Refaktorizacia; send namiesto opakovania zobrazuje varovanie

--- src/main/java/fmfi/dalekohlad/Communication/Communication.java
+++ src/main/java/fmfi/dalekohlad/Communication/Communication.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
+import fmfi.dalekohlad.InputHandling.InputConfirmation;
 import fmfi.dalekohlad.Modules.GUIModule;
 import fmfi.dalekohlad.Operations;
 import org.apache.logging.log4j.Logger;
@@ -18,24 +19,22 @@ import org.apache.logging.log4j.LogManager;
 public class Communication {
     private static final Logger lgr = LogManager.getLogger(Communication.class);
     private static Socket sock = null;
-    private static OutputStreamWriter client_out;
-    private static BufferedReader client_in;
+    private static DataOutputStream client_out;
+    private static InputStream client_in;
     private static InetSocketAddress host;
 
     private static ArrayList<GUIModule> modules = null;
     private static boolean run = true;
-    private static final int DEFAULT_BUF_LEN = 51200;
 
     private static String readString() throws IOException {
         StringBuilder sb = new StringBuilder();
-        char[] tmp = new char[DEFAULT_BUF_LEN];
-        int sz = client_in.read(tmp, 0, DEFAULT_BUF_LEN);
-        if (sz == -1) {
+        int b;
+        while ((b = client_in.read()) > 0) {
+            sb.append((char)b);
+        }
+        if (b == -1) {
             // -1 is returned when the other side exits unexpectedly, we'll treat this case in the same way as a timeout
             throw new SocketTimeoutException();
-        }
-        for (int i = 0; i < sz; i++) {
-            sb.append(tmp[i]);
         }
         return sb.toString();
     }
@@ -49,89 +48,84 @@ public class Communication {
         }
     }
 
+    private static void processUpdate(String data) {
+        if (data.startsWith("{")) {
+            JsonReader reader = new JsonReader(new StringReader(data.trim()));
+            reader.setLenient(true);
+            JsonObject json_object = JsonParser.parseReader(reader).getAsJsonObject();
+            modules.forEach(x -> x.update(json_object));
+        }
+        else if (!data.equals("")) {
+            Operations.add(data);
+        }
+    }
+
     private static void periodicUpdate() {
-        // caka na data zo serveru a posuva ich vsetkym modulom
-        String data = null;
-        lgr.debug("Entering read loop");
+        connect();
         while (run) {
-            boolean read = false;
-            while (!read && run) {
-                try {
-                    data = readString();
-                    read = true;
-                }
-                catch (SocketTimeoutException e) {
-                    reconnect();
-                }
-                catch (Exception e) {
-                    lgr.error("Failed to read data, repeating...");
-                    sleep(1000);
-                }
+            try {
+                processUpdate(readString());
             }
-            if (read) {
-                if (data.equals("")) {
-                    continue;
-                }
-                if (data.startsWith("{")) {
-                    JsonReader reader = new JsonReader(new StringReader(data.trim()));
-                    reader.setLenient(true);
-                    JsonObject json_object = JsonParser.parseReader(reader).getAsJsonObject();
-                    modules.forEach(x -> x.update(json_object));
-                }
-                else {
-                    Operations.add(data);
-                }
+            catch (SocketTimeoutException e) {
+                reconnect();
+            }
+            catch (Exception e) {
+                lgr.error("Failed to read data, repeating...");
+                sleep(1000);
             }
         }
     }
 
     private static void reconnect() {
-        lgr.debug("Trying to reconnect");
+        lgr.debug("Attempting to reconnect...");
         closeSock();
         connect();
     }
 
+    private static void sockConnect() throws IOException {
+        sock = new Socket();
+        sock.connect(host, 2000);
+        client_out = new DataOutputStream(sock.getOutputStream());
+        client_in = sock.getInputStream();
+    }
+
     private static void connect() {
-        boolean success = false;
-        while (!success) {
-            lgr.debug(String.format("Connecting to %s:%d", host.getAddress(), host.getPort()));
+        lgr.debug(String.format("Connecting to %s:%d", host.getAddress(), host.getPort()));
+        while (true) {
             try {
-                sock = new Socket();
-                sock.connect(host, 2000);
-                client_out = new OutputStreamWriter(sock.getOutputStream());
-                client_in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                success = true;
+                sockConnect();
+                return;
             } catch (Exception e) {
-                lgr.error(String.format("Connection attempt %s:%d failed, repeating",
+                lgr.error(String.format("Connection attempt to %s:%d failed, repeating...",
                         host.getAddress(), host.getPort()), e);
                 sleep(2000);
             }
         }
     }
 
-    public static Thread init(InetSocketAddress host, ArrayList<GUIModule> modules) {
-        Communication.modules = modules;
-        Communication.host = host;
-        connect();
-        Runtime.getRuntime().addShutdownHook(new Thread(Communication::close));
+    private static Thread startPeriodicUpdates() {
         Runnable runnable = Communication::periodicUpdate;
         Thread periodic_thread = new Thread(runnable);
         periodic_thread.start();
         return periodic_thread;
     }
 
+    public static Thread init(InetSocketAddress host, ArrayList<GUIModule> modules) {
+        Communication.modules = modules;
+        Communication.host = host;
+        Runtime.getRuntime().addShutdownHook(new Thread(Communication::close));
+        return startPeriodicUpdates();
+    }
+
     public static void sendData(String data) {
-        // kazdy modul odosiela informacie ked bude potrebovat
-        boolean sent = false;
-        while (!sent) {
-            try {
-                client_out.write(data);
-                client_out.flush();
-                sent = true;
-            } catch (Exception e) {
-                lgr.error("Failed to send data, repeating...");
-                sleep(2500);
-            }
+        try {
+            ByteArrayOutputStream out_data = new ByteArrayOutputStream();
+            out_data.write(data.getBytes());
+            out_data.write(0);
+            client_out.write(out_data.toByteArray(), 0, data.length()+1);
+        } catch (Exception e) {
+            lgr.error("Failed to send data:", e);
+            InputConfirmation.warn("Data couldn't be sent!");
         }
     }
 
@@ -140,7 +134,7 @@ public class Communication {
             sock.close();
         }
         catch (Exception e) {
-            lgr.error("Failed to close socket", e);
+            lgr.error("Failed to close socket:", e);
         }
     }
 
