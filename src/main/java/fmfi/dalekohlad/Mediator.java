f8ad8f6d960d2a9d677f026c72fc753d52f57623
13andrej <andrej.paluch@refis.sk>
2020-12-03 22:20:54 +0100

    refaktorizacia, odosielanie hodnoty enterom

--- src/main/java/fmfi/dalekohlad/Mediator.java
+++ src/main/java/fmfi/dalekohlad/Mediator.java
@@ -48,7 +48,7 @@ public class Mediator {
         loadScene(stage);
         bindModules(stage);
         InetSocketAddress host = loadHostFromConfig();
-        //Communication.init(host, modules);
+        Communication.init(host, modules);
     }
 
     private static void loadScene(Stage stage) {
